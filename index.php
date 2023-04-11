745e59b14e6acc8662f29cc5f78baf66ebbfcc66
vicbradley <106238421+vicbradley@users.noreply.github.com>
2023-04-11 15:07:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Example of Adding Newlines in PHP</title>
+</head>
+<body>
 
-echo "hello kakak ahyar ganteng kan ?? ";
+<?php
+echo "Nama : Vic Bradley";
+echo "<br>";
+echo "NIM : 52020002";
+echo "<br>";
+echo "Jurusan : Teknik Informatika";
+echo "<br>";
+?>
+
+</body>
+</html>
